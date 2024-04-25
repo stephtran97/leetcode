be713d115daa09d29220ee67df31544fdd0674da
Steph Tran <109913503+stephtran97@users.noreply.github.com>
2024-04-25 13:50:35 +0700

    Create find-the-difference.js

---
+++ find-the-difference.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {character}
+ */
+var findTheDifference = function(s, t) {
+    for (let i = 0; i<s.length; i++){
+        t = t.replace(s[i],'')
+    }
+    return t
+};
